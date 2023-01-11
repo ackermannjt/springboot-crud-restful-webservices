4dfaad204a7b8e49b8e46076383dd605f69cdb5d
ackermannjt <122458814+ackermannjt@users.noreply.github.com>
2023-01-11 08:54:34 -0500

    Add files via upload

---
+++ spring-crud-restful-webservices/src/main/java/com/jeffsproject/springboot/SpringCrudRestfulWebservicesApplication.java
@@ -0,0 +1,12 @@
+package com.jeffsproject.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudRestfulWebservicesApplication.class, args);
+	}
+}
